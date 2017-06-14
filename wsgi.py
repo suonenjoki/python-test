016c7239b7c307132f2bcebed20bec5333367d2d
Jarkko Suonenjoki <jaksu@iki.fi>
2017-06-14 17:43:40 +0200

    Suonenjoelta

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World from Minishift!"
+    return "Hello World from Suonenjoki!"
 
 if __name__ == "__main__":
     application.run()
