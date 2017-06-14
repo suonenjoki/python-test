23cdf5e9cfedd6e951c4652006efdf06ab41aca0
Jarkko Suonenjoki <jaksu@iki.fi>
2017-06-14 18:01:01 +0200

    Webhook

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World ja persut from Suonenjoki!"
+    return "Hello World ja Suomi from Suonenjoki!"
 
 if __name__ == "__main__":
     application.run()
